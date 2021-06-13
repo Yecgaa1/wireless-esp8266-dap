c52adb4c21bd69a689f9003887e2ba1ba5786f01
xtx <2656823393@qq.com>
2021-06-13 15:57:04 +0800

    123

--- main/wifi_configuration.h
+++ main/wifi_configuration.h
@@ -10,8 +10,8 @@
 #ifndef __WIFI_CONFIGURATION__
 #define __WIFI_CONFIGURATION__
 
-#define WIFI_SSID "DAP"
-#define WIFI_PASS "12345678"
+#define WIFI_SSID "Chino"
+#define WIFI_PASS "Lht_Yecgaa"
 
 
 #define USE_STATIC_IP 1
